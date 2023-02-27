51bfb7149c0bc387c2dfa6671be4b32c525bfc61
rickardthunborg <rickard.a.thunborg@gmail.com>
2023-02-27 20:52:52 +0100

    fixade css

--- dest/script.js
+++ dest/script.js
@@ -3,6 +3,7 @@ let form = document.querySelector("#todo-form");
 let list = document.querySelector("#todo-list");
 let input = document.querySelector("#todo-title");
 const filters = document.querySelectorAll('input[type=radio]');
+let counter = document.querySelector('#itemsLeft');
 class Todo {
     constructor(taskName) {
         this.title = taskName;
@@ -48,7 +49,6 @@ function toggleList() {
     else if (selectedFilter == "completed") {
         displayTodos(numberOfToDos.filter(x => x.completed));
     }
-    let counter = document.querySelector('#itemsLeft');
     counter.textContent = numberOfToDos.filter(x => !x.completed).length.toString();
 }
 function displayTodos(numberOfToDos) {
@@ -57,10 +57,12 @@ function displayTodos(numberOfToDos) {
     }
     numberOfToDos.forEach(x => {
         let listItem = document.createElement('li');
+        listItem.classList.add("todo-item");
         let title = document.createElement('p');
         title.textContent = x.title;
         let deleteBtn = document.createElement('button');
         let label = document.createElement('label');
+        label.classList.add("delete-btn-container");
         let checkbox = document.createElement('input');
         checkbox.setAttribute('type', 'checkbox');
         checkbox.checked = x.completed ? true : false;
@@ -70,6 +72,7 @@ function displayTodos(numberOfToDos) {
         deleteBtn.addEventListener('click', () => {
             listItem.remove();
             numberOfToDos.splice(numberOfToDos.indexOf(x), 1);
+            counter.textContent = numberOfToDos.filter(x => !x.completed).length.toString();
         });
         checkbox.addEventListener('change', () => {
             x.toggleComplete();
@@ -81,6 +84,9 @@ function displayTodos(numberOfToDos) {
         listItem.append(title);
         label.append(deleteBtn);
         listItem.append(label);
+        listItem.style.position = "relative";
+        label.style.position = "absolute";
+        label.style.right = "0";
         list === null || list === void 0 ? void 0 : list.append(listItem);
     });
 }
--- src/script.ts
+++ src/script.ts
@@ -2,6 +2,8 @@ let form = document.querySelector<HTMLFormElement>("#todo-form")!;
 let list = document.querySelector<HTMLUListElement>("#todo-list")!;
 let input = document.querySelector<HTMLInputElement>("#todo-title");
 const filters = document.querySelectorAll<HTMLInputElement>('input[type=radio]');
+let counter = document.querySelector<HTMLElement>('#itemsLeft')!;
+
 
 class Todo {
     public title: string;
@@ -70,7 +72,6 @@ if (selectedFilter == "all") {
         displayTodos(numberOfToDos.filter(x => x.completed))
     }
     
-    let counter = document.querySelector<HTMLElement>('#itemsLeft')!;
     counter.textContent = numberOfToDos.filter(x => !x.completed).length.toString();
 
 }
@@ -85,10 +86,12 @@ function displayTodos(numberOfToDos: Todo[]): void {
     numberOfToDos.forEach(x => {
 
         let listItem = document.createElement('li');
+        listItem.classList.add("todo-item");
         let title = document.createElement('p');
         title.textContent = x.title;
         let deleteBtn = document.createElement('button');
         let label = document.createElement('label')
+        label.classList.add("delete-btn-container");
         let checkbox = document.createElement('input');
         checkbox.setAttribute('type', 'checkbox');
         checkbox.checked = x.completed ? true : false;
@@ -100,7 +103,9 @@ function displayTodos(numberOfToDos: Todo[]): void {
         
         deleteBtn.addEventListener('click', () => {
             listItem.remove();
-            numberOfToDos.splice(numberOfToDos.indexOf(x), 1);        
+            numberOfToDos.splice(numberOfToDos.indexOf(x), 1); 
+            counter.textContent = numberOfToDos.filter(x => !x.completed).length.toString();
+       
         })
         
         checkbox.addEventListener('change', () => {
@@ -114,6 +119,11 @@ function displayTodos(numberOfToDos: Todo[]): void {
         listItem.append(title);
         label.append(deleteBtn);
         listItem.append(label);
+
+        listItem.style.position = "relative";
+        label.style.position = "absolute";
+        label.style.right = "0";
+    
         
         list?.append(listItem);
 
