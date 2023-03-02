9061426c76f5a6517e2ec8ac7b9a01469a7da6a7
rickardthunborg <112192254+rickardthunborg@users.noreply.github.com>
2023-03-02 13:50:55 +0100

    tests done

--- tests/todos.spec.js
+++ tests/todos.spec.js
@@ -14,12 +14,48 @@ test('add todo', async ({ page }) => {
   await expect(todoTitle).toEqual('Clean kitchen')
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('1 item left', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5501/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  let input = await page.locator('#todo-title');
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  await input.type('Random todo');
+  await page.keyboard.press('Enter');
+
+  let itemsLeft = await page.textContent('#itemsLeft');
+
+  await expect(itemsLeft).toEqual('1 item left');
+
+  let checkbox = await page.locator('.check-done');
+
+  await checkbox.click();
+
+  itemsLeft = await page.textContent('#itemsLeft');
+
+  await expect(itemsLeft).toEqual('0 items left');
+
+
+});
+
+test('2 items left', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5501/');
+
+  let input = await page.locator('#todo-title');
+
+  await input.type('Clean kitchen');
+  await page.keyboard.press('Enter')
+
+  await input.type('Eat food');
+  await page.keyboard.press('Enter')
+
+  await input.type('Feed dog');
+  await page.keyboard.press('Enter')
+
+  let checkbox = await page.locator('.check-done').last();
+  
+  await checkbox.click();
+
+  let itemsLeft = await page.textContent('#itemsLeft');
+  
+  await expect(itemsLeft).toEqual('2 items left')
 });
